17bf68d6bca2e922ae14dadffd49a5ef024f6b06
santyzenith <santyg20@gmail.com>
2025-03-29 05:20:08 +0000

    upload files

--- kafka-monitoring.py
+++ kafka-monitoring.py
@@ -2,17 +2,17 @@ from kafka import KafkaConsumer
 from prometheus_client import Counter, Histogram, start_http_server
 
 # TODO: Update the Kafka topic to the movie log of your team
-# topic = 'movielogN'
+topic = 'movielogN'
 
 start_http_server(8765)
 
 # Metrics like Counter, Gauge, Histogram, Summaries
 # Refer https://prometheus.io/docs/concepts/metric_types/ for details of each metric
 # TODO: Define metrics to show request count. Request count is total number of requests made with a particular http status
-# REQUEST_COUNT = ?(
-#     'request_count', 'Recommendation Request Count',
-#     ['http_status']
-# )
+REQUEST_COUNT = Counter(
+     'request_count', 'Recommendation Request Count',
+     ['http_status']
+)
 
 REQUEST_LATENCY = Histogram('request_latency_seconds', 'Request latency')
 
@@ -20,7 +20,7 @@ REQUEST_LATENCY = Histogram('request_latency_seconds', 'Request latency')
 def main():
     consumer = KafkaConsumer(
         topic,
-        bootstrap_servers='localhost:9092',
+        bootstrap_servers=['localhost:9092'],
         auto_offset_reset='latest',
         group_id=topic,
         enable_auto_commit=True,
@@ -35,6 +35,8 @@ def main():
             # Hint: Extract the status code from the message and use it as a label for the metric.
             # print(values) - You can print values and see how to get the status
             # status = Eg. 200,400 etc
+            status = values[3]
+            REQUEST_COUNT.labels(http_status=status).inc()
             # REQUEST_COUNT.?(status).inc()
 
             # Updating request latency histogram
